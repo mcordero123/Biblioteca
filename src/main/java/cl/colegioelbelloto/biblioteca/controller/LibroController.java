3c4f185a301fb6a84106f42a378d0faeba3f709e
Miguel <mcordero@colegioelbelloto.com>
2025-10-23 16:36:31 -0300

    Implementación de estilo de formulario

--- src/main/java/cl/colegioelbelloto/biblioteca/controller/LibroController.java
+++ src/main/java/cl/colegioelbelloto/biblioteca/controller/LibroController.java
@@ -3,6 +3,7 @@ package cl.colegioelbelloto.biblioteca.controller;
 
 import cl.colegioelbelloto.biblioteca.model.Libro;
 
+import cl.colegioelbelloto.biblioteca.services.GoogleBooksService;
 import cl.colegioelbelloto.biblioteca.services.LibroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,14 +13,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/libros")
 public class LibroController {
     @Autowired
     LibroService libroService;
+    @Autowired
+    GoogleBooksService googleBooksService;
     @PostMapping("/guardar")
     public String guardarLibro(@ModelAttribute Libro libro, Model model){
         try {
+            googleBooksService.buscarPorIsbn(libro.getIsbn());
             libroService.saveLibro(libro);
             model.addAttribute("mensaje", "libro guardado exitosamente");
             model.addAttribute("libro", new Libro());
@@ -34,5 +40,11 @@ public class LibroController {
     public String mostrarFormulario(Model model){
         model.addAttribute("libro", new Libro());
         return "libro_form";
+    }
+    @GetMapping("/lista")
+    public String listarLibros(Model model){
+        List<Libro> libros = libroService.AllLibros();
+        model.addAttribute("libros", libros);
+        return "lista_libros";
     }
 }
